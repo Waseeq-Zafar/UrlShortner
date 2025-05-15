87c5292287be3c4a5cfe4e5dce18ce2327e1947e
Waseeq <rinky.zafar@gmail.com>
2025-05-15 19:44:33 +0530

    Initial commit with project files

---
+++ url-creation-service/src/test/java/com/wz/urlcreationservice/UrlCreationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.urlcreationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlCreationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
